7cf3900e004f7cdf60375b35135c7972b4ad6a39
MrCheese556 <85973878+MrCheese556@users.noreply.github.com>
2022-01-29 17:46:45 -0500

    Full update 1

--- Build/Unity Photon MLP.framework.js
+++ Build/Unity Photon MLP.framework.js
@@ -1305,7 +1305,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3244752;
+STATICTOP = STATIC_BASE + 3246448;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3371,7 +3371,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3244752;
+var STATIC_BUMP = 3246448;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -16254,8 +16254,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 106469;
-Module["wasmMaxTableSize"] = 106469;
+Module["wasmTableSize"] = 106473;
+Module["wasmMaxTableSize"] = 106473;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
